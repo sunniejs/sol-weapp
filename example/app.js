247a704d0fe82ffd9581eb832819dc22c2d6da09
sunnie <406803045@qq.com>
2019-10-16 10:03:33 +0800

    更新提示

--- example/app.js
+++ example/app.js
@@ -1,39 +1,37 @@
 //app.js
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
+    onLaunch: function(options) {
+        if (wx.canIUse('getUpdateManager')) {
+            const updateManager = wx.getUpdateManager()
+            updateManager.onCheckForUpdate(function(res) {
+                // 请求完新版本信息的回调
+            })
+            updateManager.onUpdateReady(function() {
+                wx.showModal({
+                    title: '更新提示',
+                    content: '新版本已经准备好，是否重启应用？',
+                    success: function(res) {
+                        if (res.confirm) {
+                            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+                            updateManager.applyUpdate()
+                        } else if (res.cancel) {
+                            updateManager.applyUpdate()
+                        }
+                    }
+                })
+            })
+            updateManager.onUpdateFailed(function() {
+                // 新版本下载失败
+                console.log('onUpdateFailed')
+            })
+        } else {
+            wx.showModal({
+                title: '提示',
+                content: '您的微信版本过低，建议升级到最新版本。'
+            })
         }
-      }
-    })
-  },
-  globalData: {
-    userInfo: null
-  }
+    },
+    globalData: {
+        userInfo: null
+    }
 })
