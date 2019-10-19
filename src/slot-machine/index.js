729832ef7a2814fee1db46de3368acd66f006e53
宋楠 <406803045@qq.com>
2019-10-19 21:13:55 +0800

    老虎机

--- src/slot-machine/index.js
+++ src/slot-machine/index.js
@@ -31,70 +31,111 @@ Component({
         spinDisabled: false,
         credits: 50, //积分
         curBet: 1, // 每局消耗积分
-        firstReelStopTime:  667, // 动画停止时间
+        firstReelStopTime: 667, // 动画停止时间
         secondReelStopTime: 575,
         thirdReelStopTime: 568,
-        reels: [{ top: null }, { top: null }, { top: null }],
-        timer: []
+        reels: [{
+            top: null
+        }, {
+            top: null
+        }, {
+            top: null
+        }],
+        timer: [],
+        result: {
+            reels: ['1.0', '1.0', '1.0'],
+            prize: null,
+            success: true,
+            credits: 96,
+            dayWinnings: 69,
+            lifetimeWinnings: 69
+        }
     },
 
     /**
      * 组件的方法列表
      */
     methods: {
-        spin: function() {
-            const { credits, curBet, secondReelStopTime, thirdReelStopTime ,firstReelStopTime} = this.data
+        spin: function () {
+            const {
+                credits,
+                curBet,
+                secondReelStopTime,
+                thirdReelStopTime,
+                firstReelStopTime,
+                payoutStopTime,
+                result
+            } = this.data
             if (this.data.spinDisabled) return !1
             this.setData({
                 spinDisabled: true,
                 credits: credits - curBet
             })
+            // 开始滚动
             this.start_reel_spin(0, 0)
             this.start_reel_spin(1, 0)
             this.start_reel_spin(2, 0)
-            var b = function() {
-              var a = 0
-              window.setTimeout(function() {
-                slotMachine._stop_reel_spin(1, d.reels[0])
-              }, a);
-                (a += slotMachine.secondReelStopTime);
-                window.setTimeout(function() {
-                  slotMachine._stop_reel_spin(2, d.reels[1])
-                }, a);
-                (a += slotMachine.thirdReelStopTime);
-                window.setTimeout(function() {
-                  slotMachine._stop_reel_spin(3, d.reels[2])
-                }, a);
-                (a += slotMachine.payoutStopTime);
-                window.setTimeout(function() {
-                  slotMachine.end_spin(d)
-                }, a)
-            };
-            c = !1;
-            d = { reels: ['1.0', '1.0', '1.0'], prize: null, success: true, credits: 96, dayWinnings: 69, lifetimeWinnings: 69 };
-           // 第一个轮子停止滚动时间
-              setTimeout( () =>{
-                b()
-            }, firstReelStopTime)
-          },
-            // this.start_reel_spin(1, secondReelStopTime)
-            // this.start_reel_spin(2, secondReelStopTime + thirdReelStopTime)
+            // 结束
+            runAsync(firstReelStopTime)
+                .then(() => {
+                    this.stop_reel_spin(0, result.reels[0])
+                    return runAsync(secondReelStopTime)
+                }).then(() => {
+                    this.stop_reel_spin(1, result.reels[1])
+                    return runAsync(thirdReelStopTime)
+                }).then(() => {
+                    this.stop_reel_spin(2, result.reels[2])
+                    return runAsync(payoutStopTime)
+                }).then(() => {
+                    this.end_spin()
+
+                })
+            // var b = function () {
+            //     var a = 0
+            //     setTimeout(() => {
+            //         slotMachine.stop_reel_spin(1, d.reels[0])
+            //     }, a);
+            //     (a += slotMachine.secondReelStopTime);
+            //     setTimeout(() => {
+            //         slotMachine.stop_reel_spin(2, d.reels[1])
+            //     }, a);
+            //     (a += slotMachine.thirdReelStopTime);
+            //     setTimeout(function () {
+            //         slotMachine.stop_reel_spin(3, d.reels[2])
+            //     }, a);
+            //     (a += slotMachine.payoutStopTime);
+            //     setTimeout(function () {
+            //         slotMachine.end_spin(d)
+            //     }, a)
+            // };
+
+            // var d = {
+            //     reels: ['1.0', '1.0', '1.0'],
+            //     prize: null,
+            //     success: true,
+            //     credits: 96,
+            //     dayWinnings: 69,
+            //     lifetimeWinnings: 69
+            // };
+            // // 第一个轮子停止滚动时间
+            // console.log(firstReelStopTime)
+            // setTimeout(() => {
+            //     b()
+            // }, firstReelStopTime)
         },
         // 开始选择动画
-        start_reel_spin: function(index, b) {
-            const { stripHeight, reelSpeed1Delta } = this.data
+        start_reel_spin: function (index, b) {
+            const {
+                stripHeight,
+                reelSpeed1Delta
+            } = this.data
             const c = Date.now()
             const position = parseInt(-(Math.random() * stripHeight * 2))
             this.setData({
                 [`reels[${index}].top`]: position
             })
 
-            // var f = function() {
-            //     // ;(e += Date.now() < c + slotMachine.reelSpeed1Time + b ? slotMachine.reelSpeed1Delta : slotMachine.reelSpeed2Delta),
-            //     //     (e += a * slotMachine.reelSpeedDifference),
-            //     //     e > 0 && (e = 2 * -slotMachine.stripHeight)
-            // }
-            //   console.log(index)
+
             this.data.timer[index] = setInterval(() => {
                 this.setData({
                     [`reels[${index}].top`]: this.data.reels[index].top + reelSpeed1Delta
@@ -108,21 +149,58 @@ Component({
             // d.data('spinTimer', g)
         },
         // 停止动画
-        stop_reel_spin: function(a, b) {
-            var c = $('#reel' + a),
-                d = c.data('spinTimer')
-            if ((window.clearInterval(d), c.data('spinTimer', null), null != b)) {
-                var e = slotMachine.stripHeight / window.numIconsPerReel,
-                    f = -slotMachine.stripHeight - (b - 1) * e + slotMachine.alignmentOffset
-                c.css({ top: f - slotMachine.stripHeight }).animate(
-                    { top: f + slotMachine.bounceHeight },
-                    slotMachine.positioningTime,
-                    'linear',
-                    function() {
-                        c.animate({ top: f }, slotMachine.bounceTime, 'easeOutElastic')
-                    }
-                )
-            }
+        stop_reel_spin: function (index, lottery) {
+            const {
+                stripHeight,
+                numIconsPerReel,
+                alignmentOffset
+            } = this.data
+
+            const cellHeight = stripHeight / numIconsPerReel
+            const position = -stripHeight - lottery * cellHeight + alignmentOffset
+            this.setData({
+                [`reels[${index}].top`]: position - stripHeight
+            })
+
+
+            // if ((window.clearInterval(d), c.data('spinTimer', null), null != b)) {
+
+            // const cellHeight = stripHeight / numIconsPerReel
+            // const position = -stripHeight - lottery * cellHeight + alignmentOffset
+            // this.setData({
+            //     [`reels[${index}].top`]: position - stripHeight
+            // })
+            // c.css({
+            //     top:
+            // }).animate({
+            //         top: f + slotMachine.bounceHeight
+            //     },
+            //     slotMachine.positioningTime,
+            //     'linear',
+            //     function () {
+            //         c.animate({
+            //             top: f
+            //         }, slotMachine.bounceTime, 'easeOutElastic')
+            //     }
+            // )
+            //  }
         }
+        // this.start_reel_spin(1, secondReelStopTime)
+        // this.start_reel_spin(2, secondReelStopTime + thirdReelStopTime)
     }
+
+
 })
+/**
+ * runAsync 延迟返回 promise 方法
+ * @param  {Number} time 延迟时间
+ * @return {type}   返回Promise对象
+ */
+function runAsync(time) {
+    return new Promise(function (resolve, reject) {
+        const timer = setTimeout(function () {
+            resolve()
+            clearTimeout(timer)
+        }, time)
+    })
+}
