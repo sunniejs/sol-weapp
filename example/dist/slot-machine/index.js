778310927562ef59f78c0ba7c35e6bc946382b6d
sunnie <406803045@qq.com>
2019-10-22 18:12:50 +0800

    老虎机

--- example/dist/slot-machine/index.js
+++ example/dist/slot-machine/index.js
@@ -15,16 +15,13 @@ Component({
         }
     },
     data: {
-        stripHeight: 720,
-        alignmentOffset: 86, // 结果位置偏移量
+        stripHeight: 720, // 总高度
+        alignmentOffset: 43, // 结果位置偏移量
         payoutStopTime: 700,
-        reelSpeedDifference: 0,
-        reelSpeed1Delta: 100,
+        reelSpeed1Delta: 100, // 间隔位移
         reelSpeed1Time: 0,
-        reelSpeed2Delta: 100,
-        positioningTime: 200,
+        positioningTime: 200, // 停止前动画时间
         bounceHeight: 200,
-        bounceTime: 1e3,
         winningsFormatPrefix: '',
         soundEnabled: !0,
         sounds: {},
@@ -34,13 +31,23 @@ Component({
         firstReelStopTime: 667, // 动画停止时间
         secondReelStopTime: 575,
         thirdReelStopTime: 568,
-        reels: [{
-            top: null
-        }, {
-            top: null
-        }, {
-            top: null
-        }],
+        reels: [
+            {
+                top: -1360,
+                animation: '',
+                css: ''
+            },
+            {
+                top: -992,
+                animation: '',
+                css: ''
+            },
+            {
+                top: -1116,
+                animation: '',
+                css: ''
+            }
+        ],
         timer: [],
         result: {
             reels: ['1.0', '1.0', '1.0'],
@@ -49,23 +56,16 @@ Component({
             credits: 96,
             dayWinnings: 69,
             lifetimeWinnings: 69
-        }
+        },
+        numIconsPerReel: 6
     },
 
     /**
      * 组件的方法列表
      */
     methods: {
-        spin: function () {
-            const {
-                credits,
-                curBet,
-                secondReelStopTime,
-                thirdReelStopTime,
-                firstReelStopTime,
-                payoutStopTime,
-                result
-            } = this.data
+        spin: function() {
+            const { credits, curBet, secondReelStopTime, thirdReelStopTime, firstReelStopTime, payoutStopTime, result } = this.data
             if (this.data.spinDisabled) return !1
             this.setData({
                 spinDisabled: true,
@@ -73,23 +73,24 @@ Component({
             })
             // 开始滚动
             this.start_reel_spin(0, 0)
-            this.start_reel_spin(1, 0)
-            this.start_reel_spin(2, 0)
+            // this.start_reel_spin(1, 0)
+            // this.start_reel_spin(2, 0)
             // 结束
-            runAsync(firstReelStopTime)
-                .then(() => {
-                    this.stop_reel_spin(0, result.reels[0])
-                    return runAsync(secondReelStopTime)
-                }).then(() => {
-                    this.stop_reel_spin(1, result.reels[1])
-                    return runAsync(thirdReelStopTime)
-                }).then(() => {
-                    this.stop_reel_spin(2, result.reels[2])
-                    return runAsync(payoutStopTime)
-                }).then(() => {
-                    this.end_spin()
-
-                })
+            runAsync(firstReelStopTime).then(() => {
+                this.stop_reel_spin(0, result.reels[0])
+                return runAsync(secondReelStopTime)
+            })
+            // .then(() => {
+            //     this.stop_reel_spin(1, result.reels[1])
+            //     return runAsync(thirdReelStopTime)
+            // })
+            // .then(() => {
+            //     this.stop_reel_spin(2, result.reels[2])
+            //     return runAsync(payoutStopTime)
+            // })
+            // .then(() => {
+            //     //     this.end_spin()
+            // })
             // var b = function () {
             //     var a = 0
             //     setTimeout(() => {
@@ -124,17 +125,13 @@ Component({
             // }, firstReelStopTime)
         },
         // 开始选择动画
-        start_reel_spin: function (index, b) {
-            const {
-                stripHeight,
-                reelSpeed1Delta
-            } = this.data
+        start_reel_spin: function(index, b) {
+            const { stripHeight, reelSpeed1Delta } = this.data
             const c = Date.now()
             const position = parseInt(-(Math.random() * stripHeight * 2))
             this.setData({
                 [`reels[${index}].top`]: position
             })
-
 
             this.data.timer[index] = setInterval(() => {
                 this.setData({
@@ -149,19 +146,48 @@ Component({
             // d.data('spinTimer', g)
         },
         // 停止动画
-        stop_reel_spin: function (index, lottery) {
-            const {
-                stripHeight,
-                numIconsPerReel,
-                alignmentOffset
-            } = this.data
-
+        stop_reel_spin: function(index, lottery) {
+            const { stripHeight, numIconsPerReel, alignmentOffset, bounceHeight, positioningTime } = this.data
             const cellHeight = stripHeight / numIconsPerReel
             const position = -stripHeight - lottery * cellHeight + alignmentOffset
+            // 清除滚动timer
+            clearInterval(this.data.timer[index])
+
             this.setData({
                 [`reels[${index}].top`]: position - stripHeight
             })
 
+            var animation = wx.createAnimation({
+                transformOrigin: '50% 50%',
+                duration: positioningTime,
+                timingFunction: 'linear',
+                delay: 0
+            })
+            animation.translateY(bounceHeight).step()
+            this.setData({
+                [`reels[${index}].animation`]: animation.export()
+            })
+
+            runAsync(positioningTime).then(() => {
+                this.setData({
+                    [`reels[${index}].css`]: 'bounce'
+                })
+            })
+            // c.animate({ top: f }, 3000, 'easeOutElastic')
+            // runAsync(1000).then(() => {
+            //     var animation1 = wx.createAnimation({
+            //         transformOrigin: '50% 50%',
+            //         duration: 1000,
+            //         timingFunction: 'ease',
+            //         delay: 1000
+            //     })
+            //     animation1.translateY(-bounceHeight).step()
+            //     this.setData({
+            //         [`reels[${index}].animation`]: animation1.export()
+            //     })
+            // })
+            // animation2.translateY(bounceHeight).step()
+            // animation: easeInElastic .35s forwards;
 
             // if ((window.clearInterval(d), c.data('spinTimer', null), null != b)) {
 
@@ -188,8 +214,6 @@ Component({
         // this.start_reel_spin(1, secondReelStopTime)
         // this.start_reel_spin(2, secondReelStopTime + thirdReelStopTime)
     }
-
-
 })
 /**
  * runAsync 延迟返回 promise 方法
@@ -197,8 +221,8 @@ Component({
  * @return {type}   返回Promise对象
  */
 function runAsync(time) {
-    return new Promise(function (resolve, reject) {
-        const timer = setTimeout(function () {
+    return new Promise(function(resolve, reject) {
+        const timer = setTimeout(function() {
             resolve()
             clearTimeout(timer)
         }, time)
